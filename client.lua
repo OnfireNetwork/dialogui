3e91a95670e89e8458acfdbcb69222f41494cc14
JanHolger <jan@bebendorf.eu>
2019-10-27 21:05:05 +0100

    !!!BREAKING CHANGES!!! Now supports multiple columns for buttons and inputs and the select input now has a size property to allow scrollable lists

--- client.lua
+++ client.lua
@@ -5,60 +5,111 @@ SetWebURL(web, "http://asset/dialogui/dialog.html")
 local nextId = 1
 local dialogs = {}
 local lastOpened = -1
-function createDialog(title, text, ...)
+function createDialog(title, text)
     local id = nextId
     nextId = nextId + 1
     dialogs[id] = {
         title = title,
         text = text,
-        buttons = {...},
-        inputs = {},
+        columns = {},
         variables = {}
     }
     return id
 end
-function addDialogSelect(dialog, label, ...)
+function setDialogButtons(dialog, column, ...)
     if dialogs[dialog] == nil then
         return
     end
-    table.insert(dialogs[dialog].inputs, {
+    if dialogs[dialog].columns[column] == nil then
+        dialogs[dialog].columns[column] = {
+            inputs = {},
+            buttons = {}
+        }
+    end
+    dialogs[dialog].columns[column].buttons = {...}
+end
+function addDialogSelect(dialog, column, label, size, ...)
+    if dialogs[dialog] == nil then
+        return
+    end
+    if dialogs[dialog].columns[column] == nil then
+        dialogs[dialog].columns[column] = {
+            inputs = {},
+            buttons = {}
+        }
+    end
+    table.insert(dialogs[dialog].columns[column].inputs, {
         type = "select",
         name = label,
+        size = size,
+        labelMode = false,
         options = {...}
     })
-    return #dialogs[dialog].inputs
+    return #dialogs[dialog].columns[column].inputs
 end
-function addDialogCheckbox(dialog, label)
+function addDialogCheckbox(dialog, column, label)
     if dialogs[dialog] == nil then
         return
     end
-    table.insert(dialogs[dialog].inputs, {
+    if dialogs[dialog].columns[column] == nil then
+        dialogs[dialog].columns[column] = {
+            inputs = {},
+            buttons = {}
+        }
+    end
+    table.insert(dialogs[dialog].columns[column].inputs, {
         type = "checkbox",
         name = label
     })
-    return #dialogs[dialog].inputs
+    return #dialogs[dialog].columns[column].inputs
 end
-function setDialogSelectOptions(dialog, input, ...)
+function setDialogSelectOptions(dialog, column, input, ...)
     if dialogs[dialog] == nil then
         return
     end
-    if dialogs[dialog].inputs[input] == nil then
+    if dialogs[dialog].columns[column] == nil then
         return
     end
-    if dialogs[dialog].inputs[input].options == nil then
+    if dialogs[dialog].columns[column].inputs[input] == nil then
         return
     end
-    dialogs[dialog].inputs[input].options = {...}
+    dialogs[dialog].columns[column].inputs[input].labelMode = false
+    if dialogs[dialog].columns[column].inputs[input].options == nil then
+        return
+    end
+    dialogs[dialog].columns[column].inputs[input].options = {...}
 end
-function addDialogTextInput(dialog, label)
+function setDialogSelectOptionsWithLabels(dialog, column, input, options)
     if dialogs[dialog] == nil then
         return
     end
-    table.insert(dialogs[dialog].inputs, {
+    if dialogs[dialog].columns[column] == nil then
+        return
+    end
+    if dialogs[dialog].columns[column].inputs[input] == nil then
+        return
+    end
+    dialogs[dialog].columns[column].inputs[input].labelMode = true
+    if dialogs[dialog].columns[column].inputs[input].options == nil then
+        return
+    end
+    dialogs[dialog].columns[column].inputs[input].options = options
+end
+function addDialogTextInput(dialog, column, label)
+    if dialogs[dialog] == nil then
+        return
+    end
+    if dialogs[dialog].columns[column] == nil then
+        dialogs[dialog].columns[column] = {
+            inputs = {},
+            buttons = {}
+        }
+    end
+    table.insert(dialogs[dialog].columns[column].inputs, {
         type = "text",
         name = label
     })
-    return #dialogs[dialog].inputs
+    return #dialogs[dialog].columns[column].inputs
 end
 function setVariable(dialog, name, value)
     if dialogs[dialog] == nil then
@@ -99,33 +150,61 @@ function showDialog(dialog)
     if d.text ~= nil then
         json = json.."text:\""..replaceVariables(d.text, d.variables).."\","
     end
-    if d.inputs ~= nil then
-        json = json.."inputs:["
-        for i=1,#d.inputs do
-            if i > 1 then
-                json = json..","
+    json = json.."columns:["
+    if #d.columns > 0 then
+        for j=1,#d.columns do
+            if j > 1 then
+                json = json..",{"
+            else
+                json = json.."{"
             end
-            json = json.."{type:\""..d.inputs[i].type.."\",name:\""..replaceVariables(d.inputs[i].name, d.variables).."\""
-            if d.inputs[i].options ~= nil then
-                json = json..",options:["
-                for j=1,#d.inputs[i].options do
-                    if j > 1 then
+            if d.columns[j].inputs ~= nil then
+                json = json.."inputs:["
+                for i=1,#d.columns[j].inputs do
+                    if i > 1 then
                         json = json..","
                     end
-                    json = json.."\""..d.inputs[i].options[j].."\""
+                    json = json.."{type:\""..d.columns[j].inputs[i].type.."\",name:\""..replaceVariables(d.columns[j].inputs[i].name, d.variables).."\""
+                    if d.columns[j].inputs[i].options ~= nil then
+                        if d.columns[j].inputs[i].labelMode then
+                            json = json..",options:{"
+                            local firstOne = true
+                            for k,v in pairs(d.columns[j].inputs[i].options) do
+                                if not firstOne then
+                                    json = json..","
+                                else
+                                    firstOne = false
+                                end
+                                json = json..k..":\""..v.."\""
+                            end
+                            json = json.."}"
+                        else
+                            json = json..",options:["
+                            for k=1,#d.columns[j].inputs[i].options do
+                                if k > 1 then
+                                    json = json..","
+                                end
+                                json = json.."\""..d.columns[j].inputs[i].options[k].."\""
+                            end
+                            json = json.."]"
+                        end
+                    end
+                    if d.columns[j].inputs[i].size ~= nil then
+                        json = json..",size:"..d.columns[j].inputs[i].size
+                    end
+                    json = json.."}"
                 end
-                json = json.."]"
+                json = json.."],"
             end
-            json = json.."}"
+            json = json.."buttons:["
+            for i=1,#d.columns[j].buttons do
+                if i > 1 then
+                    json = json..","
+                end
+                json = json.."\""..replaceVariables(d.columns[j].buttons[i], d.variables).."\""
+            end
+            json = json.."]}"
         end
-        json = json.."],"
-    end
-    json = json.."buttons:["
-    for i=1,#d.buttons do
-        if i > 1 then
-            json = json..","
-        end
-        json = json.."\""..replaceVariables(d.buttons[i], d.variables).."\""
     end
     ExecuteWebJS(web, "SetDialog("..dialog..",{"..json.."]});")
     SetIgnoreLookInput(true)
@@ -141,6 +220,7 @@ AddEvent("__dialog_system_closed", function()
     SetInputMode(INPUT_GAME)
 end)
 AddFunctionExport("create", createDialog)
+AddFunctionExport("setButtons", setDialogButtons)
 AddFunctionExport("addSelect", addDialogSelect)
 AddFunctionExport("addTextInput", addDialogTextInput)
 AddFunctionExport("addCheckbox", addDialogCheckbox)
@@ -149,3 +229,4 @@ AddFunctionExport("show", showDialog)
 AddFunctionExport("close", closeDialog)
 AddFunctionExport("destroy", destroyDialog)
 AddFunctionExport("setSelectOptions", setDialogSelectOptions)
+AddFunctionExport("setSelectLabeledOptions", setDialogSelectOptionsWithLabels)
---
+++ dialog.js
@@ -0,0 +1,154 @@
+function SetDialog(id, json) {
+    let menuDiv = document.getElementById("menu");
+    menuDiv.style.display = "inline-block";
+    let inputs = [];
+    let nextButtonId = 1;
+    while (menuDiv.firstChild !== null) {
+        menuDiv.removeChild(menuDiv.firstChild);
+    }
+    let menuContentDiv = document.createElement("div");
+    menuContentDiv.style.width = (json.columns.length*300)+"px";;
+    if (json.title !== undefined) {
+        if(json.title.length > 0){
+            menuContentDiv.appendChild(document.createElement("hr"));
+            let titleH = document.createElement("h1");
+            titleH.className = "menu-title";
+            titleH.appendChild(document.createTextNode(json.title));
+            menuContentDiv.appendChild(titleH);
+            menuContentDiv.appendChild(document.createElement("hr"));
+        }
+    }
+    if (json.text !== undefined) {
+        if(json.text.length > 0){
+            let textSpan = document.createElement("span");
+            textSpan.className = "menu-info";
+            textSpan.appendChild(document.createTextNode(json.text));
+            menuContentDiv.appendChild(textSpan);
+            menuContentDiv.appendChild(document.createElement("hr"));
+        }
+    }
+    let gridElement = document.createElement("div");
+    gridElement.className = "row";
+    gridElement.style.width = (json.columns.length*300)+"px";
+    gridElement.style.marginLeft = "0px";
+    for(let colId=0; colId<json.columns.length; colId++){
+        let column = json.columns[colId];
+        let colElement = document.createElement("div");
+        switch(json.columns.length){
+            case 1:
+                colElement.className = "col-md-12";
+                break;
+            case 2:
+                colElement.className = "col-md-6";
+                break;
+            case 3:
+                colElement.className = "col-md-4";
+                break;
+            case 4:
+                colElement.className = "col-md-3";
+                break;
+        }
+        if (column.inputs !== undefined && column.inputs.length > 0) {
+            let rendered = 0;
+            for (let jsonInput of column.inputs){
+                if (jsonInput.type === "text") {
+                    let inputElement = document.createElement("input");
+                    inputElement.className = "menu-input-text";
+                    if(jsonInput.name.length > 0){
+                        let labelElement = document.createElement("label");
+                        labelElement.className = "menu-label";
+                        labelElement.appendChild(document.createTextNode(jsonInput.name + ':'));
+                        colElement.appendChild(labelElement);
+                        colElement.appendChild(inputElement);
+                        rendered++;
+                    }
+                    inputs.push(inputElement);
+                }
+                if (jsonInput.type === "select") {
+                    let selectElement = document.createElement("select");
+                    selectElement.className = "menu-select";
+                    if(jsonInput.size !== undefined){
+                        selectElement.size = jsonInput.size;
+                    }
+                    if(Array.isArray(jsonInput.options)){
+                        for (let option of jsonInput.options) {
+                            let optionElement = document.createElement("option");
+                            optionElement.appendChild(document.createTextNode(option));
+                            selectElement.appendChild(optionElement);
+                        }
+                    }else{
+                        for (let optionKey of Object.keys(jsonInput.options)) {
+                            let optionElement = document.createElement("option");
+                            optionElement.appendChild(document.createTextNode(jsonInput.options[optionKey]));
+                            optionElement.value = optionKey;
+                            selectElement.appendChild(optionElement);
+                        }
+                    }
+                    if(jsonInput.name === undefined || jsonInput.name.length > 0){
+                        if(jsonInput.name !== undefined){
+                            let labelElement = document.createElement("label");
+                            labelElement.className = "menu-label";
+                            labelElement.appendChild(document.createTextNode(jsonInput.name + ':'));
+                            colElement.appendChild(labelElement);
+                        }
+                        colElement.appendChild(selectElement);
+                        rendered++;
+                    }
+                    inputs.push(selectElement);
+                }
+                if (jsonInput.type === "checkbox") {
+                    let checkboxElement = document.createElement("input");
+                    checkboxElement.setAttribute("type", "checkbox");
+                    checkboxElement.className = "menu-checkbox";
+                    if(jsonInput.name.length > 0){
+                        colElement.appendChild(checkboxElement);
+                        let labelElement = document.createElement("label");
+                        labelElement.className = "menu-label";
+                        labelElement.appendChild(document.createTextNode(jsonInput.name));
+                        colElement.appendChild(labelElement);
+                        colElement.appendChild(document.createElement("br"));
+                        rendered++;
+                    }
+                    inputs.push(checkboxElement);
+                }
+            }
+            if(rendered > 0){
+                colElement.appendChild(document.createElement("hr"));
+            }
+        }
+        for (let i = 0; i < column.buttons.length; i++){
+            if(column.buttons[i].length == 0){
+                continue;
+            }
+            let buttonElement = document.createElement("button");
+            buttonElement.className = "menu-button";
+            buttonElement.appendChild(document.createTextNode(column.buttons[i]));
+            let buttonId = nextButtonId;
+            nextButtonId++;
+            buttonElement.onclick = function () {
+                let params = [id, buttonId];
+                for (let input of inputs) {
+                    if (input.type === "checkbox") {
+                        params.push(input.checked);
+                    } else {
+                        params.push(input.value);
+                    }
+                }
+                if(json.autoclose === undefined || json.autoclose){
+                    CloseDialog();
+                    ue.game.callevent("__dialog_system_closed", "[]");
+                }
+                ue.game.callevent("OnDialogSubmit", JSON.stringify(params));
+            };
+            colElement.appendChild(buttonElement);
+        }
+        gridElement.appendChild(colElement);
+    }
+    menuContentDiv.appendChild(gridElement);
+    menuDiv.appendChild(menuContentDiv);
+}
+
+function CloseDialog() {
+    let menuDiv = document.getElementById("menu");
+    menuDiv.style.display = "none";
+}
